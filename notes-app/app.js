c1169c6d0070b3d599264b52db55c647b72c2ba2
Phil <birkal@outlook.com>
2014-12-24 17:48:29 -0800

    Add notes app

---
+++ notes-app/app.js
@@ -0,0 +1,16 @@
+angular
+  .module('notesApp', [])
+  .controller('MainCtrl', [function() {
+    var self = this;
+
+    self.notes = [
+      {id: 1, label: 'First Note', done: false},
+      {id: 2, label: 'Second Note', done: false},
+      {id: 3, label: 'Done Note', done: true},
+      {id: 4, label: 'Last Note', done: false}
+    ];
+
+    self.deleteNote = function(noteIndex) {
+      self.notes.splice(noteIndex, 1);
+    };
+  }]);
